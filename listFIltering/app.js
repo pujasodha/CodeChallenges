67c4af1dbbe19f0970faa9f6e94a00181ff8aa42
pujasodha <pujasodha@gmail.com>
2019-01-05 12:59:36 -0800

    i think i updated list filtering

--- listFIltering/app.js
+++ listFIltering/app.js
@@ -5,12 +5,54 @@ function filter_list(l) {
     var num = [];
 
     //for loop for type of element - this one being a number - to loop through given array to find the numbers to push to the array
-    for (var i = 0; i<l.length; i++) {
-        if(typeof (l[i]) === 'number') {
-            num.push(l[i])
+    for (var i = 0; i < l.length; i++) {
+        if (typeof l[i] === 'number') {
+            num.push(l[i]);
         }
     }
     //return the new array
-    return num 
+    return num;
 }
 
+// ================================================ ONLINE ANSWERS ========================================= //
+function filter_list(l) {
+    return l.filter(function(v) {
+        return typeof v == 'number';
+    });
+}
+
+// =========================================== //
+
+function filter_list(l) {
+    return l.filter((v) => typeof v == 'number');
+}
+
+// =========================================== //
+
+const filter_list = (l) => l.filter((c) => typeof c === 'number');
+
+// =========================================== //// ==== //
+
+function filter_list(l) {
+    return l.filter((e) => Number.isInteger(e));
+}
+
+// =========================================== //// ==== //
+
+function filter_list(l) {
+    return l.filter(function(elem) {
+        return typeof elem != 'string';
+    });
+}
+
+// =========================================== //
+
+function filter_list(l) {
+    var res = [];
+    for (var item = 0; item < l.length; item++) {
+        if (typeof l[item] === 'number') {
+            res.push(l[item]);
+        }
+    }
+    return res;
+}
